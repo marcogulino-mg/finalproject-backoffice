6f7e013496036611160e29a762f41d2cea115381
marcogulino-mg <marcogulino@gmail.com>
2025-07-14 16:11:48 +0200

    first commit

---
+++ src/main/java/com/marco/finalproject/finalproject_backoffice/FinalprojectBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.marco.finalproject.finalproject_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalprojectBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marco/finalproject/finalproject_backoffice/FinalprojectBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.finalproject.finalproject_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
