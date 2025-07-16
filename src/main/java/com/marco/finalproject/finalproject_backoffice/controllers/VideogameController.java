47a36c3937709c11b51de4bebc4f1340520f74e5
marcogulino-mg <marcogulino@gmail.com>
2025-07-16 16:09:09 +0200

    feat(videogame):   - Added show and delete method in VideogameController   - Added existsById, deleteById, findById and getById methods in VideogameService.
    feat(layout):
      - Created error/404 template
      - Added link in fragments/header for videogame index CRUD
      - Created videogames/show template
      - Added btn in videogames/index for delete CRUD

--- src/main/java/com/marco/finalproject/finalproject_backoffice/controllers/VideogameController.java
+++ src/main/java/com/marco/finalproject/finalproject_backoffice/controllers/VideogameController.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.marco.finalproject.finalproject_backoffice.models.Videogame;
 import com.marco.finalproject.finalproject_backoffice.services.VideogameService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/videogames")
@@ -25,5 +29,21 @@ public class VideogameController {
         List<Videogame> videogames = videogameService.findAll();
         model.addAttribute("videogames", videogames);
         return "videogames/index";
+    }
+
+    // INFO: SHOW
+    @GetMapping("/{id}")
+    public String show(@PathVariable int id, Model model) {
+        model.addAttribute("videogame", videogameService.getById(id));
+        return "videogames/show";
+    }
+
+    // INFO: DELETE
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable int id, Model model, HttpServletResponse response) {
+        if (!videogameService.deleteById(id)) {
+            return "errors/404";
+        }
+        return "redirect:/videogames";
     }
 }
--- src/main/java/com/marco/finalproject/finalproject_backoffice/services/VideogameService.java
+++ src/main/java/com/marco/finalproject/finalproject_backoffice/services/VideogameService.java
@@ -1,6 +1,7 @@
 package com.marco.finalproject.finalproject_backoffice.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,39 @@ public class VideogameService {
     @Autowired
     private VideogameRepo videogameRepo;
 
-    // Show every videogames
+    // Explanation: Check if a videogame exists
+    public Boolean existsById(int id) {
+        return videogameRepo.existsById(id);
+    }
+
+    // Explanation: Show every videogames
     public List<Videogame> findAll() {
         return videogameRepo.findAll();
     }
+
+    // Explanation: Find single videogame using id
+    public Optional<Videogame> findById(int id) {
+        return videogameRepo.findById(id);
+    }
+
+    // Explanation: Check if the videogame exists
+    public Videogame getById(int id) {
+        Optional<Videogame> vgAttempt = findById(id);
+        if (vgAttempt.isEmpty()) {
+            // throw new RuntimeException("Missing Videogame");
+        }
+
+        return vgAttempt.get();
+    }
+
+    // Explanation: Delete by ID
+    public Boolean deleteById(int id) {
+        if (existsById(id)) {
+            videogameRepo.deleteById(id);
+            return true;
+        }
+
+        return false;
+    }
+
 }
