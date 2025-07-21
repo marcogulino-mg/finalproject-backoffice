910b85d5a09e8200db4ceb11109edfca03199763
marcogulino-mg <marcogulino@gmail.com>
2025-07-21 17:34:23 +0200

    feat(console):   - created REST API

---
+++ src/main/java/com/marco/finalproject/finalproject_backoffice/api/ConsoleApiController.java
@@ -0,0 +1,76 @@
+package com.marco.finalproject.finalproject_backoffice.api;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marco.finalproject.finalproject_backoffice.models.Console;
+import com.marco.finalproject.finalproject_backoffice.services.ConsoleService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/consoles")
+public class ConsoleApiController {
+
+    @Autowired
+    private ConsoleService consoleService;
+
+    // INFO: READ
+    @GetMapping
+    public List<Console> index() {
+        List<Console> consoles = consoleService.findAll();
+        return consoles;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Console> show(@PathVariable int id) {
+        Optional<Console> consoleAttempt = consoleService.findById(id);
+        if (consoleAttempt.isEmpty()) {
+            return new ResponseEntity<Console>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Console>(consoleAttempt.get(), HttpStatus.OK);
+    }
+
+    // INFO: CREATE
+    @PostMapping
+    public ResponseEntity<Console> store(@Valid @RequestBody Console console) {
+        consoleService.create(console);
+        return new ResponseEntity<Console>(console, HttpStatus.OK);
+    }
+
+    // INFO: UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<Console> update(@Valid @RequestBody Console console, @PathVariable int id) {
+        if (consoleService.existsById(id)) {
+            console.setId(id);
+            consoleService.create(console);
+            return new ResponseEntity<Console>(console, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<Console>(HttpStatus.NOT_FOUND);
+    }
+
+    // INFO: DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Console> delete(@PathVariable int id) {
+        if (consoleService.existsById(id)) {
+            consoleService.deleteById(id);
+            return new ResponseEntity<Console>(HttpStatus.OK);
+        }
+        return new ResponseEntity<Console>(HttpStatus.NOT_FOUND);
+
+    }
+}
--- src/main/java/com/marco/finalproject/finalproject_backoffice/api/VideogameApiController.java
+++ src/main/java/com/marco/finalproject/finalproject_backoffice/api/VideogameApiController.java
@@ -4,9 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
