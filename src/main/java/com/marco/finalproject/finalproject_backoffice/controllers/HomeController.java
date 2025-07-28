b71a6481fe9707ff053d4a1ee8567fe50e693bfa
marco-gulino <marcogulino98@gmail.com>
2025-07-28 21:30:54 +0200

    feat(layout):   - change text from it to eng   - add btn for /create in videogames and consoles

--- src/main/java/com/marco/finalproject/finalproject_backoffice/controllers/HomeController.java
+++ src/main/java/com/marco/finalproject/finalproject_backoffice/controllers/HomeController.java
@@ -1,14 +1,14 @@
-package com.marco.finalproject.finalproject_backoffice.controllers;
+// package com.marco.finalproject.finalproject_backoffice.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/")
-public class HomeController {
-    @GetMapping
-    public String home() {
-        return "home";
-    }
-}
+// @Controller
+// @RequestMapping("/")
+// public class HomeController {
+// @GetMapping
+// public String home() {
+// return "home";
+// }
+// }
